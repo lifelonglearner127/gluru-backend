d9a5854d35f3c3a2c85c5be960a8fdad7755530b
lifelonglearner127 <life.long.learner127@outlook.com>
2018-10-24 06:19:18 +0400

    update user admin model

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from profiles.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class SupportUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'email', 'first_name',
+        'last_name', 'is_staff', 'is_active'
+    )
+    ordering = ('-id',)
+    search_fields = ('email',)
+    readonly_fields = ('created_at',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': (
+            'first_name', 'last_name', 'phone_number'
+        )}),
+        ('Permissions', {'fields': ('is_staff', )}),
+        ('Additional Info', {'fields': (
+            'is_active', 'created_at', 'idp_uuid'
+        )}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    filter_horizontal = ()
+    list_filter = ()
--- profiles/models.py
+++ profiles/models.py
@@ -8,34 +8,23 @@ from django.contrib.auth.models import (
 )
 from django.db import models
 
-from tickets.constants import UUID_MAX_LENGTH
-
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, email, password=None):
-        """
-        Create and return a `User` with an email, username and password.
-        """
-        if username is None:
-            raise TypeError('Users must have a username.')
-
+    def create_user(self, email, password=None):
         if email is None:
             raise TypeError('Users must have an email address.')
 
-        user = self.model(username=username, email=self.normalize_email(email))
+        user = self.model(email=self.normalize_email(email))
         user.set_password(password)
         user.save()
 
         return user
 
-    def create_superuser(self, username, email, password):
-        """
-        Create and return a `User` with superuser powers.
-        """
+    def create_superuser(self, email, password):
         if password is None:
             raise TypeError('Superusers must have a password.')
 
-        user = self.create_user(username, email, password)
+        user = self.create_user(email, password)
         user.is_superuser = True
         user.is_staff = True
         user.save()
@@ -44,40 +33,40 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(
-        db_index=True,
+    first_name = models.CharField(
         max_length=255,
-        unique=True
+        blank=True
     )
-
+    last_name = models.CharField(
+        max_length=255,
+        blank=True
+    )
     email = models.EmailField(
         db_index=True,
         unique=True
     )
-
-    uuid = models.CharField(
-        max_length=UUID_MAX_LENGTH,
+    phone_number = models.CharField(
+        max_length=30,
+        blank=True
+    )
+    idp_uuid = models.CharField(
+        max_length=255,
         blank=True,
     )
-
     is_active = models.BooleanField(
         default=True
     )
-
     is_staff = models.BooleanField(
         default=False
     )
-
     created_at = models.DateTimeField(
         auto_now_add=True
     )
-
     updated_at = models.DateTimeField(
         auto_now=True
     )
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
 
     objects = UserManager()
 
@@ -89,26 +78,12 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self._generate_jwt_token()
 
     def get_full_name(self):
-        """
-        This method is required by Django for things like handling emails.
-        Typically, this would be the user's first and last name. Since we do
-        not store the user's real name, we return their username instead.
-        """
-        return self.username
+        return self.first_name + ' ' + self.last_name
 
     def get_short_name(self):
-        """
-        This method is required by Django for things like handling emails.
-        Typically, this would be the user's first name. Since we do not store
-        the user's real name, we return their username instead.
-        """
-        return self.username
+        return self.first_name
 
     def _generate_jwt_token(self):
-        """
-        Generates a JSON Web Token that stores this user's ID and has an expiry
-        date set to 60 days into the future.
-        """
         dt = datetime.now() + timedelta(days=60)
 
         token = jwt.encode({
