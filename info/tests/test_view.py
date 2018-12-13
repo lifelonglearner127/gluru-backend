e7462b070ed812ccbe503dc08a8748f2cdb0fdf3
lifelonglearner127 <life.long.learner127@outlook.com>
2018-12-14 00:27:17 +0400

    info; add product test case

--- info/tests/test_view.py
+++ info/tests/test_view.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from profiles.models import User
-from info.models import GluuServer, GluuOS
+from info.models import GluuServer, GluuOS, GluuProduct
 
 
 class GluuServerViewSetTest(APITestCase):
@@ -291,7 +291,145 @@ class GluuOSViewSetTest(APITestCase):
 
 
 class GluuProductViewsetTest(APITestCase):
-    pass
+    def setUp(self):
+
+        self.admin = User.objects.create_superuser(
+            email='admin@gluu.org',
+            password='admin'
+        )
+
+        self.user = User.objects.create_user(
+            email='user@gluu.org',
+            password='user'
+        )
+
+        self.valid_payload = {
+            "product": {
+                "name": "Super Gluu"
+            }
+        }
+
+        self.invalid_payload = {
+            "product": {
+                "name": ""
+            }
+        }
+
+        self.product = GluuProduct.objects.create(name='Oxd')
+
+    def test_create_info_by_not_manager(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user.token)
+        response = self.client.post(
+            reverse('info:product-list'),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_info_valid(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin.token)
+        response = self.client.post(
+            reverse('info:product-list'),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_info_invalid(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin.token)
+        response = self.client.post(
+            reverse('info:product-list'),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_info_by_not_manager(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user.token)
+        response = self.client.put(
+            reverse('info:product-detail', kwargs={'pk': self.product.id}),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_update_info_valid(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin.token)
+        response = self.client.put(
+            reverse('info:product-detail', kwargs={'pk': self.product.id}),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_info_invalid(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin.token)
+        response = self.client.put(
+            reverse('info:product-detail', kwargs={'pk': self.product.id}),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_non_existing(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin.token)
+        response = self.client.put(
+            reverse('info:product-detail', kwargs={'pk': 30}),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_retrieve_info_valid(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin.token)
+        response = self.client.get(
+            reverse('info:product-detail', kwargs={'pk': self.product.id}),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_info_invalid(self):
+        response = self.client.get(
+            reverse('info:product-detail', kwargs={'pk': 30}),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_info_by_not_manager(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user.token)
+        response = self.client.delete(
+            reverse('info:product-detail', kwargs={'pk': self.product.id}),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_info_valid(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin.token)
+        response = self.client.delete(
+            reverse('info:product-detail', kwargs={'pk': self.product.id}),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_info_invalid(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin.token)
+        response = self.client.delete(
+            reverse('info:product-detail', kwargs={'pk': 30}),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
 class TicketCategoryViewSetTest(APITestCase):
