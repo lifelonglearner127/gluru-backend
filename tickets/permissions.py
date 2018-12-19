34a3e0dfc6fce392ee078b516f5de2cdc11cc3f3
lifelonglearner127 <life.long.learner127@outlook.com>
2018-12-20 03:58:51 +0400

    update ticket app permission rule

--- tickets/permissions.py
+++ tickets/permissions.py
@@ -46,12 +46,6 @@ class TicketCustomPermission(permissions.BasePermission):
             return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        if obj.company_association is None:
-            if request.method in ['GET']:
-                return True
-            if request.method in ['PUT', 'DELETE']:
-                return request.user == obj.created_by
-
         if request.user.is_superuser:
             return True
 
@@ -63,9 +57,17 @@ class TicketCustomPermission(permissions.BasePermission):
                 permission_name=view.action
             )
 
-        membership = request.user.membership_set.filter(
-            company=obj.company_association
-        ).first()
+        if obj.company_association is None:
+            if request.method in ['GET']:
+                return True
+            if request.method in ['PUT', 'DELETE']:
+                return request.user == obj.created_by
+
+        membership = None
+        if request.user.is_authenticated:
+            membership = request.user.membership_set.filter(
+                company=obj.company_association
+            ).first()
 
         return membership and membership.role and\
             membership.role.has_permission(
@@ -77,6 +79,19 @@ class TicketCustomPermission(permissions.BasePermission):
 
 class AnswerCustomPermission(permissions.BasePermission):
     def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
+
+        if request.user.is_staff:
+            staff_role = UserRole.objects.get(name='staff')
+            model_name = 'Ticket' if view.action == 'retrieve' else\
+                'Answer'
+            return staff_role.has_permission(
+                app_name='tickets',
+                model_name=model_name,
+                permission_name=view.action
+            )
+
         kwargs = request.parser_context.get('kwargs')
         ticket_pk = kwargs.get('ticket_pk', None)
         if ticket_pk is None:
@@ -84,27 +99,45 @@ class AnswerCustomPermission(permissions.BasePermission):
 
         ticket = get_object_or_404(Ticket, is_deleted=False, pk=ticket_pk)
 
-        staff_role = UserRole.objects.get(name='staff')
+        if ticket.company_association:
+            if request.user.is_authenticated:
+                membership = request.user.membership_set.filter(
+                    company=ticket.company_association
+                ).first()
 
-        if ticket.company_association is None:
-            if request.user.is_superuser:
-                return True
-
-            if request.user.is_staff:
                 model_name = 'Ticket' if view.action == 'retrieve' else\
                     'Answer'
-                return staff_role.has_permission(
-                    app_name='tickets',
-                    model_name=model_name,
-                    permission_name=view.action
-                )
-
-            return ticket.created_by == request.user
-        else:
-            if not request.user.is_authenticated:
+                return membership and membership.role and\
+                    membership.role.has_permission(
+                        app_name='tickets',
+                        model_name=model_name,
+                        permission_name=view.action
+                    )
+            else:
                 return False
+        else:
+            return request.method in ['GET'] or request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if obj.created_by == request.user:
+            return True
+
+        if request.user.is_superuser:
+            return True
+
+        if request.user.is_staff:
+            staff_role = UserRole.objects.get(name='staff')
+            model_name = 'Ticket' if view.action == 'retrieve' else\
+                'Answer'
+            return staff_role.has_permission(
+                app_name='tickets',
+                model_name=model_name,
+                permission_name=view.action
+            )
+
+        if obj.ticket.company_association:
             membership = request.user.membership_set.filter(
-                company=ticket.company_association
+                company=obj.ticket.company_association
             ).first()
 
             model_name = 'Ticket' if view.action == 'retrieve' else 'Answer'
@@ -115,5 +148,4 @@ class AnswerCustomPermission(permissions.BasePermission):
                     permission_name=view.action
                 )
 
-    def has_object_permission(self, request, view, obj):
-        return True
+        return False
